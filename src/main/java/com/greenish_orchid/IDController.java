43c118429d4ab354d23f0fd83d660010a5867964
cfc <chenfacai@1705ha.com>
2019-09-04 21:08:13 +0800

    lsdfhsdbf

---
+++ src/main/java/com/greenish_orchid/IDController.java
@@ -0,0 +1,18 @@
+package com.greenish_orchid;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author CFC
+ *
+ *	时间2019年9月4日
+ */
+@Controller
+public class IDController {
+
+	
+	public void list(){
+		System.out.println("skdfsdfmsbdfnsbdmfbsmndbf");
+		System.out.println("s,djnfsndfnsdfnsdn");
+	}
+}
